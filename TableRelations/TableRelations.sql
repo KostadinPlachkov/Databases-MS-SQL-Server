067c22440db64589c989539d907d9e94f7a5e7cc
KostadinPlachkov <kostt23@gmail.com>
2017-07-28 17:58:57 +0300

    Added: Many-to-Many, One-to-One New encoding: Unicode(with UTF-8)

--- TableRelations/TableRelations.sql
+++ TableRelations/TableRelations.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE DBTest
+﻿CREATE DATABASE DBTest
 
 USE DBTest
 
@@ -23,4 +23,63 @@ INSERT INTO Peaks VALUES('Musala', 1)
 SELECT * FROM Peaks
 SELECT * FROM Mountains
 
---
+-- Many-to-Many
+CREATE TABLE Employees(
+	Id INT PRIMARY KEY IDENTITY(1, 1),
+	FirstName VARCHAR(50) NOT NULL,
+	LastName VARCHAR(50) NOT NULL,
+	Salary MONEY NOT NULL
+)
+
+CREATE TABLE Projects(
+	Id INT PRIMARY KEY IDENTITY(1, 1),
+	Name VARCHAR(255) NOT NULL,
+	Description VARCHAR(MAX),
+	StartDate DATE DEFAULT GETDATE(),
+	EndDate DATE
+)
+
+-- Mapping Table
+CREATE TABLE EmployeesProjects(
+	EmployeeId INT FOREIGN KEY REFERENCES Employees(Id),
+	ProjectId INT FOREIGN KEY REFERENCES Projects(Id),
+	CONSTRAINT PK_EmployeeId_ProjectId
+	PRIMARY KEY(EmployeeId, ProjectId)
+)
+
+INSERT INTO Employees VALUES('John', 'Cena', 100000)
+INSERT INTO Projects(Name) VALUES('Strong AI')
+
+SELECT * FROM Employees
+SELECT * FROM Projects
+
+INSERT INTO EmployeesProjects VALUES(1, 1)
+SELECT * FROM EmployeesProjects
+
+-- One-to-One
+CREATE TABLE Cars(
+	Id INT PRIMARY KEY IDENTITY(1, 1),
+	Model VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Drivers(
+	Id INT PRIMARY KEY IDENTITY(1, 1),
+	Name VARCHAR(50) NOT NULL,
+	CarId INT UNIQUE,
+	CONSTRAINT FK_Cars
+	FOREIGN KEY(CarId)
+	REFERENCES Cars(Id)
+)
+
+INSERT INTO Cars VALUES('Tesla')
+INSERT INTO Drivers(Name) VALUES('Elon Musk')
+INSERT INTO Drivers(Name, CarId) VALUES('John Cena', 1)
+
+SELECT * FROM Drivers
+
+-- Test for One-to-One
+UPDATE Drivers
+SET CarId = 1
+WHERE Id = 1
+
+-- Example
